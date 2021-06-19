a5d75bb840102c69f53440f3becc176e2667596a
Ashok Mane <ashokmane81800@gmail.com>
2021-06-19 09:14:01 +0530

    [ashok] added test curl request for employee pay roll app

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollDTO.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+public class EmployeePayrollDTO {
+	
+	public String name;
+	public long salary;
+	
+	public EmployeePayrollDTO(String name, long salary) {
+		this.name = name;
+		this.salary = salary;
+		
+	}
+	
+}
