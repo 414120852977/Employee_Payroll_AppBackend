77976ba6516b07fbabd8ca77110d744d986f9417
Ashok Mane <ashokmane81800@gmail.com>
2021-06-25 06:39:30 +0530

    [ashok] added environment to application properties for a employee payroll

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,13 +2,16 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppApplication {
 		public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee payroll started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee payroll started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 }
